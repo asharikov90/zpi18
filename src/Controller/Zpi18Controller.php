5a7fa4caf38b958e2d520577c6955eb51fdd9042
asharikov <mail@asharikov.ru>
2023-06-12 23:40:48 +0300

    Test

--- src/Controller/Zpi18Controller.php
+++ src/Controller/Zpi18Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\ScheduleService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,7 +12,7 @@ use TelegramBot\Api\BotApi;
 class Zpi18Controller extends AbstractController
 {
     #[Route(path: '/pidor', name: 'app_zpi18_pidorbot')]
-    public function pidorBot()
+    public function pidorBot(): Response
     {
         $data = json_decode(file_get_contents('php://input'), true);
         $telegramBot = new BotApi($_ENV['TELEGRAM_BOT_TOKEN_ZPI18_PIDOR']);
@@ -30,6 +31,30 @@ class Zpi18Controller extends AbstractController
                 ];
                 $rand = rand(0, count($pidors));
                 $telegramBot->sendMessage($chatId, 'И пидор дня - '.$pidors[$rand]);
+
+                return new Response('ok', headers: $headers);
+            } else {
+                return new Response('Unknown command', Response::HTTP_UNPROCESSABLE_ENTITY, $headers);
+            }
+        }
+
+        return new Response('Empty body', Response::HTTP_UNPROCESSABLE_ENTITY, $headers);
+    }
+
+    #[Route(path: '/schedule', name: 'app_zpi18_schedule')]
+    public function schedule(ScheduleService $scheduleService): Response
+    {
+        $headers = ['content-Type' => 'text/json'];
+
+        if (!empty($data['message']['text'])) {
+            $chatId = $data['message']['from']['id'];
+            $text = trim($data['message']['text']);
+
+
+            if (str_contains($text, '/rasp')) {
+                $telegramBot = new BotApi($_ENV['TELEGRAM_BOT_TOKEN_ZPI18_PIDOR']);
+                $schedule = $scheduleService->getSchedule(date('Y-m-d'));
+                $telegramBot->sendMessage($chatId, implode('\n', $schedule));
 
                 return new Response('ok', headers: $headers);
             } else {
---
+++ src/Service/ScheduleService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Service;
+
+readonly class ScheduleService
+{
+    public function __construct(private int $groupId)
+    {
+    }
+
+    public function getSchedule(string $date): array
+    {
+        $url = 'https://stud.mgri.ru/api/Rasp?idGroup='.$this->groupId.'&sdate='.$date;
+        $response = json_decode(file_get_contents($url), true);
+
+        return array_map(fn (array $raspString): string => $this->getScheduleString($raspString), $response);
+    }
+
+    private function getScheduleString(array $rasp): string
+    {
+        $result = [];
+
+        $result[] = $rasp['дисциплина'];
+        $result[] = $rasp['аудитория'];
+        $result[] = $rasp['преподаватель'];
+
+        return $rasp['начало'].'-'.$rasp['конец'].'\n'.implode('\n', $result);
+    }
+}
